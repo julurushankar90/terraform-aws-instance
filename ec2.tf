a3191853f96fdacd8b365a340e6230b11798de79
julurushankar90 <julurushankar90@gmail.com>
2026-09-14 21:54:20 +0530

    first commit

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "this" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = var.sg_ids
 
   tags = {
     Name = "terraform"
@@ -9,27 +9,3 @@ resource "aws_instance" "example" {
   }
 }
     
-resource "aws_security_group" "allow_tls" {
-  name        = "allow-all-terraform" # this is for aws account
-  description = "Allow TLS inbound traffic and all outbound traffic"
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow-all-terraform"
-  }
-}
